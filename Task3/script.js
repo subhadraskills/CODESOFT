d87bdcd4adfe392b047b890a013f2a445e9f7ee9
subhadraskills <148532127+subhadraskills@users.noreply.github.com>
2024-01-08 23:38:03 +0530

    Add files via upload

---
+++ Task3/script.js
@@ -0,0 +1,24 @@
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+  }
+  
+  function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = displayValue;
+  }
+  
+  function calculate() {
+    try {
+      displayValue = eval(displayValue);
+      document.getElementById('display').value = displayValue;
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+    }
+  }
+  
+  function backspace() {
+    displayValue = displayValue.slice(0, -1);
+    document.getElementById('display').value = displayValue;
+  }
+  
